cb704e102278701db46c99e9508778557e19e4de
lelehyeok <limorenoha@ittepic.edu.mx>
2026-07-27 21:02:10 -0700

    feat (auth): Login de bibliotecario hecho

--- BookFlow/js/script.js
+++ BookFlow/js/script.js
@@ -1,23 +1,37 @@
 const spURL = 'https://kzsxksciruyrlcjkqfqm.supabase.co';
 const spK = 'sb_publishable_OkJX5qDnmbMmHgOMRILz8Q_1TVu0IEA';
 
+//Variables 
 const sp = window.supabase.createClient(spURL, spK);
+const formLogin = document.querySelector('.login-form');
 
-async function cargarLibros() {
-    try {
-        const { data, error } = await sp
-            .from('libros') 
-            .select('*');
-        if (error) {
-            console.error("Hubo un problema al consultar la base de datos:", error.message);
-            return;
-        }
+//Eventos y acciones
+formLogin.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const user = document.getElementById('user').value;
+    const ps = document.getElementById('password').value;
 
-        console.log("¡Conexión exitosa a Supabase!");
-        console.log("Datos obtenidos:", data);
-    } catch (err) {
-        console.error("Error inesperado en el sistema:", err);
+    const { data: perfil, error: errorbusqueda } = await sp 
+        .from('bibliotecarios')
+        .select('correo')
+        .eq('usuario', user)
+        .single();
+    
+    if(errorbusqueda || !perfil){
+        alert("Usuario o contraseña incorrectos");
+        return;
     }
-}
 
-cargarLibros();
+
+    const {data,error} = await sp.auth.signInWithPassword({
+        email: perfil.correo,
+        password: ps
+    });
+
+    if(error){
+        alert("Usuario o contraseña incorrecta");
+    }else {
+        window.location.href = "principal.html";
+    }
+
+})
